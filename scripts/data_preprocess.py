b1cc9e3ae39c0255c0aa86bfbae993f8faf3ec00
S.Mohammad Mousavi <smmousavisp@gmail.com>
2020-05-31 10:50:02 +0430

    Make data_preprocessing.py file more pep8 friendly

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def get_data(file): 
 
+def get_data(file):
     data = pd.read_csv(file)
 
     feat_raw = data['quizzes']
@@ -13,25 +13,22 @@ def get_data(file):
     label = []
 
     for i in feat_raw:
-    
-        x = np.array([int(j) for j in i]).reshape((9,9,1))
+        x = np.array([int(j) for j in i]).reshape((9, 9, 1))
         feat.append(x)
-    
+
     feat = np.array(feat)
     feat = feat/9
-    feat -= .5    
-    
+    feat -= .5
+
     for i in label_raw:
-    
-        x = np.array([int(j) for j in i]).reshape((81,1)) - 1
-        label.append(x)   
-    
+        x = np.array([int(j) for j in i]).reshape((81, 1)) - 1
+        label.append(x)
+
     label = np.array(label)
-    
-    del(feat_raw)
-    del(label_raw)    
+
+    del feat_raw
+    del label_raw
 
     x_train, x_test, y_train, y_test = train_test_split(feat, label, test_size=0.2, random_state=42)
-    
-    return x_train, x_test, y_train, y_test
 
+    return x_train, x_test, y_train, y_test
